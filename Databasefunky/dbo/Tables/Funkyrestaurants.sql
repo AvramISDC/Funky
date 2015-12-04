126abe3a423d892401210697a6c0c90d356956c8
David Sebastian <rocker_seba89@yahoo.com>
2015-12-04 18:07:02 +0200

    Add database

---
+++ Databasefunky/dbo/Tables/Funkyrestaurants.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Funkyrestaurants] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (100) NOT NULL,
+    [Description]  NVARCHAR (MAX) NOT NULL,
+    [Adress]       NCHAR (100)    NOT NULL,
+    [AverageStars] FLOAT (53)     NULL
+);
+
---
+++ Databasefunky/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [ID]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NULL,
+    [Age]      INT            NOT NULL,
+    [Birthday] DATETIME       NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
