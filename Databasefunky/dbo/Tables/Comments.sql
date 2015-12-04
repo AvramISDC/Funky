a2470ac9f7aa220d8bbb6f6cced2b3f67f7b8ac6
David Sebastian <rocker_seba89@yahoo.com>
2015-12-04 18:37:12 +0200

    Update database added comments table and extra columns on Users

---
+++ Databasefunky/dbo/Tables/Comments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Comments] (
+    [ID]            INT             NOT NULL,
+    [Text]          NVARCHAR (300)  NOT NULL,
+    [User ID]       INT             NOT NULL,
+    [Restaurant ID] INT             NOT NULL,
+    [Ratings]       NVARCHAR (1000) NOT NULL,
+    CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
--- Databasefunky/dbo/Tables/Users.sql
+++ Databasefunky/dbo/Tables/Users.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [ID]       INT            IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (MAX) NULL,
-    [Age]      INT            NOT NULL,
-    [Birthday] DATETIME       NOT NULL,
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (100) NULL,
+    [Birthday]     DATETIME       NOT NULL,
+    [Email Adress] NVARCHAR (MAX) NOT NULL,
+    [Username]     NVARCHAR (MAX) NOT NULL,
+    [Password]     NVARCHAR (30)  NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
