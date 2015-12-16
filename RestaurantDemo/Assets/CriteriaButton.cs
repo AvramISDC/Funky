f525bd781b3a6a3e4fc5c0c0bdca3c3c11a03022
VortezLeon <alin_faierfox2@yahoo.com>
2015-12-16 19:18:56 +0200

    Homescreen Create

---
+++ RestaurantDemo/Assets/CriteriaButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CriteriaButton : MonoBehaviour {
+
+    public GameObject criteriaPanel;
+    public GameObject restaurantsPanel;
+
+	public void OnClick() {
+        criteriaPanel.active = !criteriaPanel.active;
+        restaurantsPanel.active = !restaurantsPanel.active;
+    }
+}
