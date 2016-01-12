e91c047a07b55d651e93e4f3458d1dc078164200
David Sebastian <rocker_seba89@yahoo.com>
2016-01-12 17:53:37 +0200

    Added RegisterUser

---
+++ funkyrestaurants.web/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using funkyrestaurants.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace funkyrestaurants.web.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public bool RegisterUser(RegisterData data)
+        {
+            var db = new FunkyDb();
+            var user = new User();
+            user.Name = data.username;
+            user.Password = data.password;
+            user.EmailAdress = data.emailadress;
+            user.Birthday = DateTime.Today;
+            db.Users.Add(user);
+            db.SaveChanges();
+            return true;
+        }
+
+
+    }
+    public class RegisterData
+    {
+        public string username;
+        public string password;
+        public string emailadress;
+
+
+    }
+}
