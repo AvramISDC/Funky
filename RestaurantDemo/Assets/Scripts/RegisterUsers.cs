b26109db936b1fdefa6fdcb70e710de5895f0d62
VortezLeon <alin_faierfox2@yahoo.com>
2016-01-29 18:19:49 +0200

    tried to fix scripts

--- RestaurantDemo/Assets/Scripts/RegisterUsers.cs
+++ RestaurantDemo/Assets/Scripts/RegisterUsers.cs
@@ -3,18 +3,43 @@ using System.Collections;
 
 public class RegisterUsers : MonoBehaviour {
 
-    static IEnumerator RegisterUser ()
+    private string username;
+    private string password;
+    private string email;
+
+    public void GetUsername(string usernameInput)
+    {
+        username = usernameInput;
+    }
+    public void GetPassword(string passwordInput)
+    {
+        password = passwordInput;
+    }
+    public void GetEmail(string emailInput)
+    {
+        email = emailInput;
+    }
+    public void Check()
+    {
+        Debug.Log(username + "," + password + "," + email);
+    }
+
+
+    public IEnumerator RegisterUser ()
     {
         WWWForm p = new WWWForm();
-        p.AddField("username", "test");
-        p.AddField("password", "test");
-        p.AddField("emailadress", "testadd.com");
+        p.AddField("username", username);
+        p.AddField("password", password);
+        p.AddField("emailadress", email);
 
         p.headers["Content-Type"] = "application/x-www-form-urlencoded"; ;
 
         WWW www = new WWW("http://localhost:53313/api/Users", p);
         yield return www;
-        Debug.Log(www.error);
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+        }
     }
 
 
