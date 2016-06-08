e2b39018079039251867e8f11e1ae8ad413a7ff4
VortezLeon <alin_faierfox2@yahoo.com>
2016-06-08 23:13:49 +0300

    Added mobile controller if needed in 3dview just disable the new controller and enable the old one

---
+++ RestaurantDemo/Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour
+{
+
+    void Update()
+    {
+        float speed = 2;
+        float xInput = Input.acceleration.x;
+        float yInput = Input.acceleration.y;
+        float zInput = Input.acceleration.z;
+        if(yInput >= 0.1)
+        {
+            transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+        if (yInput <= -0.08)
+        {
+            transform.Translate(0, 0, -Time.deltaTime * speed);
+        }
+        if (xInput >= 0.08)
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0);
+        }
+        if (xInput <= -0.08)
+        {
+            transform.Translate(-Time.deltaTime * speed, 0, 0);
+        }
+        //if (zInput >= 0.1 || xInput >= 0.1 || yInput >= 0.1)
+        //{
+        //    transform.Translate(0, 0, yInput);
+        //    transform.Translate(xInput, 0, 0);
+        //}
+        //transform.Translate(0, y, 0);
+        //transform.Rotate(0, yInput, zInput);
+    }
+}
---
+++ RestaurantDemo/Assets/Scripts/Swipe.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swipe : MonoBehaviour {
+
+    public Transform cube;
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                cube.transform.Rotate(0f, touch0.deltaPosition.x, 0f);
+            }
+
+        }
+    }
+}
